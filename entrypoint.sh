77d06767f057ccec15e061ac047b99a350c88baf
Jalzn <jalzn@Jalmirs-MacBook-Air.local>
2026-05-09 00:26:26 -0300

    fix: detect dns resolver from resolv.conf at startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
-envsubst '${BACKEND_HOST} ${FRONTEND_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+RESOLVER_ADDR=$(awk '/^nameserver/ {print $2; exit}' /etc/resolv.conf)
+export RESOLVER_ADDR
+
+echo "Detected resolver: $RESOLVER_ADDR"
+cat /etc/resolv.conf
+
+envsubst '${BACKEND_HOST} ${FRONTEND_HOST} ${RESOLVER_ADDR}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
